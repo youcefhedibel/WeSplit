ac4ddc79c2821ae099a4832795b313ee5dde24df
youcefhedibel <youyou_dz1@live.fr>
2022-08-22 09:51:50 +0100

    first commit

---
+++ webSplitt/webSplittApp.swift
@@ -0,0 +1,17 @@
+//
+//  webSplittApp.swift
+//  webSplitt
+//
+//  Created by mac on 22/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct webSplittApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
