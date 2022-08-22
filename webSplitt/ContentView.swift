51802fb119023ca08967dcb7694bbfcba6cc1408
youcefhedibel <youyou_dz1@live.fr>
2022-08-22 10:32:23 +0100

    reading text from the user

--- webSplitt/ContentView.swift
+++ webSplitt/ContentView.swift
@@ -8,9 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 10.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Form{
+            Section{
+                TextField("Amount", value: $checkAmount, format: .currency(code: "DZD") )
+            }
+            
+            Section{
+                Text(checkAmount, format: .currency(code: "DZD"))
+            }
+        }
     }
 }
 
